3e47238f8b6a8b443eeb30b19451adf31a8c3318
nolasaint <evanbailey122@gmail.com>
2024-02-06 23:26:51 -0500

    Geomspace implementation

--- pkg/algorithm/geomspace.go
+++ pkg/algorithm/geomspace.go
@@ -1,15 +1,19 @@
 package algorithm
 
-// import "math"
+import "math"
 
 // create a list of n logarithmically-spaced numbers between start and stop
 // similar to Logspace, except outputs are between [start, stop], not [base**start, base**stop]
 // if start > stop, list will be ascending, else it will be descending
 func Geomspace(start float64, stop float64, n uint32) []float64 {
+	logStart := math.Log10(start)
+	logStop := math.Log10(stop)
 	numbers := make([]float64, n)
-	// for i := 0; i < int(n); i++ {
-	// 	exponent := start + (float64(i) * (stop - start) / (float64(n) - 1))
-	// 	numbers[int(i)] = math.Pow(base, exponent)
-	// }
+
+	for i := 0; i < int(n); i++ {
+		exponent := logStart + (float64(i) * (logStop - logStart) / (float64(n) - 1))
+		numbers[int(i)] = math.Pow(10, exponent)
+	}
+
 	return numbers
 }
--- pkg/algorithm/geomspace_test.go
+++ pkg/algorithm/geomspace_test.go
@@ -16,4 +16,24 @@ func TestGeomspaceNoElements(t *testing.T) {
 	asserts.Float64SliceEquals(expected, observed, t)
 }
 
-//TODO:finish writing tests
+func TestGeomspaceAscending(t *testing.T) {
+	expected := []float64{10, 17.78279, 31.62278, 56.23413, 100}
+	observed := Geomspace(10, 100, 5)
+
+	for i := 0; i < len(observed); i++ {
+		observed[i] = toFixed(observed[i], 5)
+	}
+
+	asserts.Float64SliceEquals(expected, observed, t)
+}
+
+func TestGeomspaceDescending(t *testing.T) {
+	expected := []float64{100, 56.23413, 31.62278, 17.78279, 10}
+	observed := Geomspace(100, 10, 5)
+
+	for i := 0; i < len(observed); i++ {
+		observed[i] = toFixed(observed[i], 5)
+	}
+
+	asserts.Float64SliceEquals(expected, observed, t)
+}
